7ded210cdf6a805c7cddc19f983763fb9381ee2d
Apprentice <apprentice@devbootcamp.com>
2013-04-24 09:57:21 -0700

    sandboxagain

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,19 @@
 get '/' do
-  # Look in app/views/index.erb
+  puts "THIS CAUGHT ALL!"
+  puts "LOG: Getting /"
+  puts "LOG Params: #{params.inspect}"
   erb :index
 end
+
+get '/cool_url/:id' do
+  puts 'LOG: Getting /cool_url'
+  puts "LOG: Params: #{params.inspect}"
+  erb :get_cool_url
+end
+
+post '/cool_url/' do
+  puts "LOG: Posting to /cool_url"
+  puts "LOG Params: #{params.inspect}"
+  erb :post_cool_url
+end
+
